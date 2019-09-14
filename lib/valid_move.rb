926091195a69d5a5922321a91e2c8e4975562f2c
Jo Onuma <joimportant@yahoo.co.uk>
2019-09-14 11:52:08 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -4,13 +4,13 @@ def valid_move?(board, index)
     if board[index] == " " || board[index] ==  ""  # to return true for " ", ""
       return true
     else board[index] == "X" || board[index] == "O"  #return false for "X" & "O"
-      return false
+      return false or nil
      end
    end
 if index.between?(0,8)
    position_taken?(board, index)
    return true
 else
-   return false
+   return false or nil
 end
 end
