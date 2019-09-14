a6eaf1a34786185309155663492acc127b15bf59
Jo Onuma <joimportant@yahoo.co.uk>
2019-09-14 12:01:09 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -10,7 +10,7 @@ def valid_move?(board, index)
 if index.between?(0,8)
   position_taken?(board, index)
   return true
-else index.between?(9, 100)
+else index.between?(9,100)
   return false
   end
 end
